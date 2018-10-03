a66bdfa606ebdcafb5e3c4a0c75329ddd42c79c2
Sakhiwo Mkwebula <sakhiwo@thedigitalacademy.co.za>
2018-10-03 16:16:58 +0200

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import * as express from 'express'
+import * as bodyParser from 'body-parser'
+import {Pokemons} from "./routes/pokemons"
+
+class App {
+    public app: express.Application
+    public pokeRoutes: Pokemons = new Pokemons()
+
+    constructor() {
+        this.app = express()
+        this.config()
+        this.pokeRoutes.routes(this.app)
+    }
+
+    private config(): void {
+        //support application/json type post data 
+        this.app.use(bodyParser.json())
+        //suppport application/x-www-form-urlencoded post data 
+        this.app.use(bodyParser.urlencoded({
+            extended: false
+        }))
+    }
+}
+
+
+export default new App().app
